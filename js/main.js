4511ee9379b82099f17953d83b3a5b240fd82ad2
root <ryan@9wizards.com>
2014-09-08 09:17:28 -0700

    Reset js call for dateRangeSlider to default

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-  $("#timeline").dateRangeSlider({
-
-    bounds: {min: new Date(2014, 0, 1), max: new Date(2014, 11, 31, 20, 59, 59)},
-    defaultValues: {min: new Date(2014, 7, 15), max: new Date(2014, 20, 31)},
-//    valueLabels: "change",
-    scales: [{
-      first: function(value){ return value; },
-      end: function(value) {return value; },
-      next: function(value){
-        var next = new Date(value);
-        return new Date(next.setMonth(value.getMonth() + 1));
-      },
-      label: function(value){
-        return months[value.getMonth()];
-      },
-      format: function(tickContainer, tickStart, tickEnd){
-        tickContainer.addClass("myCustomClass");
-      }
-    }]
-  });
+  $("#timeline").dateRangeSlider();
   
   var largeSpinConfig = {
     lines: 13, // The number of lines to draw
