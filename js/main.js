78ead332aabaa8a14aa8a7fcaa623021be3b70e4
root <ryan@9wizards.com>
2014-09-08 12:48:03 -0700

    Live archive list from active jobs list

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
 jQuery(document).ready(function($){
+    // load active jobs on pageload
+    getActiveJobs();
+
+    // function to get active jobs JSON object
+    function getActiveJobs() {
+
+        $.ajax({
+            type: "GET",
+            url: "http://blue.a.blocktech.com:3000/alexandria/v1/twitter/get/activejobs",
+            success: function (responseData) {
+
+                // respond with this alert (data passed back is responseData)
+                //
+                
+                var data = $.parseJSON(responseData);
+
+                for (var i = 0; i < data.length; i++) {
+                    $("#archiveList").append('<li><a href="#" onclick="showTweetList($(this))"><span>' + data[i] + '</span> <span>1234</span></a></li>');
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                alert("some error");
+                console.log(XMLHttpRequest);
+                console.log(textStatus);
+                console.log(errorThrown);
+                $("#archiveList").append("<li class='responseRow'>Cannot connect to Librarian</li>");
+           }
+        });
+    }
 	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 	$("#timeline").dateRangeSlider({
 		bounds: {min: new Date(2014, 0, 1), max: new Date(2014, 11, 31, 20, 59, 59)},
@@ -48,43 +77,6 @@ jQuery(document).ready(function($){
   WordCloud(document.getElementById('wordCloud'), { list:cloudlist } );
 */
 
-    // function to get active jobs JSON object
-    function getActiveJobs() {
-
-        $.ajax({
-            type: "GET",
-            url: "http://blue.a.blocktech.com:4000/test/getArchivedTweets",
-            success: function (responseData) {
-
-                // respond with this alert (data passed back is responseData)
-               
-                var data = $.parseJSON(responseData);
-
-                console.log(data);
-
-                for (var i = 0; i < data.length; i++) {
-                    $("#tweetList").append('<li class = "responseRow"><div><strong>@' + data[i].p.twitter.data[6] + '</strong> <span class="tweet-date">' + data[i].p.twitter.data[1] + '</span></div> <div>' + data[i].p.twitter.data[7] + '</div></li>');
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                alert("some error");
-                console.log(XMLHttpRequest);
-                console.log(textStatus);
-                console.log(errorThrown);
-                $("#tweetList").append("<li class = 'responseRow'>Cannot connect to Librarian</li>");
-           }
-        });
-
-    }
-
-	$('#archiveList a').click(function(){
-		// load active jobs
-		var archiveTitle = $(this).find('span:first-child').text();
-		$('header input[type="search"]').attr('value',archiveTitle);
-		getActiveJobs();
-		$('#tweetListView').fadeIn(100);
-	});
-
 	$(document).on("keyup", function (e) {
 		var code = e.keyCode || e.which; if (code == 27) {
 			clearModal();
@@ -101,3 +93,36 @@ jQuery(document).ready(function($){
 		$("#tweetList li.responseRow").remove();
 	}
 });
+function showTweetList(arch){
+	var archiveTitle = $(arch).find('span:first-child').text();
+	$('header input[type="search"]').attr('value',archiveTitle);
+	getArchive();
+	$('#tweetListView').fadeIn(100);
+}
+// function to get archives JSON object
+function getArchive() {
+
+	$.ajax({
+		type: "GET",
+		url: "http://blue.a.blocktech.com:4000/test/getArchivedTweets",
+		success: function (responseData) {
+
+			// respond with this alert (data passed back is responseData)
+		   
+			var data = $.parseJSON(responseData);
+
+			console.log(data);
+
+			for (var i = 0; i < data.length; i++) {
+				$("#tweetList").append('<li class = "responseRow"><div><strong>@' + data[i].p.twitter.data[6] + '</strong> <span class="tweet-date">' + data[i].p.twitter.data[1] + '</span></div> <div>' + data[i].p.twitter.data[7] + '</div></li>');
+			}
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+			alert("some error");
+			console.log(XMLHttpRequest);
+			console.log(textStatus);
+			console.log(errorThrown);
+			$("#tweetList").append("<li class = 'responseRow'>Error</li>");
+	   }
+	});
+}
