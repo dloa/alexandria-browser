cf9c4957b0f5a1fc48b998c2107389fb1a98e02e
copernicus-mogley <ryan@9wizards.com>
2014-09-29 10:42:28 -0400

    Better alphabetical sort function

--- js/main.js
+++ js/main.js
@@ -338,33 +338,12 @@ jQuery.fn.sortElements = (function(){
 })();
 // Alphabetical sort
 function sortUnorderedList(ul, sortDescending) {
-  if(typeof ul == "string")
-    ul = document.getElementById(ul);
-
-  // Idiot-proof, remove if you want
-  if(!ul) {
-    alert("The UL object is null!");
-    return;
-  }
-
-  // Get the list items and setup an array for sorting
-  var lis = ul.getElementsByTagName("LI");
-  var vals = [];
-
-  // Populate the array
-  for(var i = 0, l = lis.length; i < l; i++)
-    vals.push(lis[i].outerHTML);
-
-  // Sort it
-  vals.sort();
-
-  // Sometimes you gotta DESC
-  if(sortDescending)
-    vals.reverse();
-
-  // Change the list on the page
-  for(var i = 0, l = lis.length; i < l; i++)
-    lis[i].outerHTML = vals[i];
+	var mylist = $('#'+ul);
+	var listitems = mylist.children('li').get();
+	listitems.sort(function(a, b) {
+	   return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
+	})
+	$.each(listitems, function(idx, itm) { mylist.append(itm); });
 }
 
 // Spinner configuration
