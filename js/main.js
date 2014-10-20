f9c6ab13269fb34091307796d193468fb4e86880
image <image@localhost.localdomain>
2014-10-19 20:44:37 -0700

    Toying with the volume bars

--- js/main.js
+++ js/main.js
@@ -425,6 +425,62 @@ function getArchiveVolume(arch) {
 					$('#'+currentView.slice(0,-8)+'Cloud').hide().css('z-index','3');
 				}
 				$('.view-link').fadeIn(fadeTimer);
+				// Volume Bars
+
+
+			//Width and height
+			var w = window.innerWidth-1;
+			var h = 100;
+			var barPadding = 1;
+			
+			var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+							11, 12, 15, 20, 18, 17, 16, 18, 23, 25, 30, 7, 14, 12, 27 ];
+			
+			//Create SVG element
+			var svg = d3.select("body")
+						.append("svg")
+						.attr("width", w)
+						.attr("id","volume")
+						.attr("height", h);
+
+			svg.selectAll("rect")
+			   .data(dataset)
+			   .enter()
+			   .append("rect")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length);
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 3);
+			   })
+			   .attr("width", w / dataset.length - barPadding)
+			   .attr("height", function(d) {
+			   		return d * 3;
+			   })
+			   .attr("fill", function(d) {
+					return "rgb(0, 0, " + (d * 10) + ")";
+			   });
+
+			svg.selectAll("text")
+			   .data(dataset)
+			   .enter()
+			   .append("text")
+			   .text(function(d) {
+			   		return d;
+			   })
+			   .attr("text-anchor", "middle")
+			   .attr("x", function(d, i) {
+			   		return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+			   })
+			   .attr("y", function(d) {
+			   		return h - (d * 3) + 14;
+			   })
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "11px")
+			   .attr("fill", "white");
+
+
+			
 				$('#wait').fadeOut(fadeTimer);
 				$('.search').attr('disabled',false);
 			}
