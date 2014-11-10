f61424aae51500172f6b1c79bb5c6f339742a74c
copernicus-mogley <ryan@9wizards.com>
2014-11-10 14:16:45 -0800

    Better Librarian archiving error reporting

--- js/main.js
+++ js/main.js
@@ -738,10 +738,11 @@ function volumeBars(arch, word, interval){
 			var largest = Math.max.apply(Math, dataset);
 			var mostRecent = Math.max.apply(Math, Object.keys(data));
 			// Difference between most recent and current time
-			console.log('Diff = '+((Date.parse(datetime)/1000)-mostRecent));
-			if ( ( ((Date.parse(datetime)/1000)-mostRecent) > (interval+90) ) && (arch = '*') ) {
+			if ( ( ( ( Date.parse(datetime)/1000 ) - mostRecent ) > ( interval*1.1 ) ) && (arch == '*') ) {
 				alert('Librarian stopped archiving!');
 			}else{
+				console.log('Diff = '+((Date.parse(datetime)/1000)-mostRecent));
+				console.log('interval = '+interval);
 				console.log('Librarian apears to be archiving');
 			}
 			var firstTimestamp = Math.min.apply(Math, Object.keys(data));
