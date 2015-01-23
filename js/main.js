011652a8992ea7034e390e3da53d34c1923ff88a
copernicus-mogley <ryan@9wizards.com>
2015-01-22 23:05:44 -0800

    SVG image replacement with SVGs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function($){
+	replaceSVG();
 	$('#spritz-container').hide();
 	$('#wait').fadeIn(fadeTimer);
 	$('#disabler').fadeIn(fadeTimer);
@@ -58,6 +59,7 @@ jQuery(document).ready(function($){
 			}
 			$('main').fadeOut(fadeTimer);
 			$('.sharing-ui').fadeOut(fadeTimer);
+			$('.view-media-ui').fadeOut(fadeTimer);
 			$('#intro').fadeIn(fadeTimer);
 			$('.twitter-archive').fadeIn(fadeTimer);
 			$('#search').fadeIn(fadeTimer);
@@ -359,7 +361,7 @@ jQuery(document).ready(function($){
 			$('#serverID').text('Dev');
 		}		
 	});
-
+	
 }); // End Document.Ready
 
 // DEFAULT VARIABLES
@@ -1700,6 +1702,44 @@ function sortUnorderedList(ul, sortDescending) {
 	   return $(a).text().toUpperCase().localeCompare($(b).text().toUpperCase());
 	})
 	$.each(listitems, function(idx, itm) { mylist.append(itm); });
+}
+
+/*
+* Replace all SVG images with inline SVG
+*/
+function replaceSVG() {
+	jQuery('.makeChildrenSVG').each(function(){
+		jQuery(this).children('img').each(function(){
+			jQuery(this).addClass('makesvg');
+		});
+	});
+	jQuery('img.makesvg').each(function(){
+		var $img = jQuery(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		jQuery.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = jQuery(data).find('svg');
+	
+			// Add replaced image's ID to the new SVG
+			if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+	
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+			
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+
+		}, 'xml');
+	});
 }
 
 // Spinner configuration
