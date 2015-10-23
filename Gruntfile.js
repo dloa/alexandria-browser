3c74f9a58adbd38881caf2ce33f8bc01b8fa88a2
Niv Sardi <xaiki@evilgiggle.com>
2015-10-23 17:06:03 -0300

    add version
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,6 +121,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		nwjs: {
 			options: {
+				version: '0.12.3',
 				build_dir: './build', // Where the build version of my node-webkit app is saved
 				keep_nw: true,
 				embed_nw: false,
