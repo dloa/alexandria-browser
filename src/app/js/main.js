239aa1a2c08c4f93df81608e2ab8a5c672a47226
copernicus-mogley <ryan@9wizards.com>
2016-04-29 00:47:33 +0200

    Link for publishing @ alexandria.io/publisher/

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -15,6 +15,7 @@ if (location.protocol == 'app:') {
 	$('.webOnly').css('display','inline-block');
 	$('.appOnly').remove();
 	var wallet;
+	$('#publish-module-link').removeAttr('onclick').attr('href','http://alexandria.io/publisher/');
 }
 
 var prevTipAmount = '';
