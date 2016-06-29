c0a86c4c58f5bf3a3c9b95acb0abb81571c0e7ca
Chris Chrysostom <chris.chrysostom@gmail.com>
2016-06-29 00:22:11 -0600

    Set payproc URLs to use https protocol all of the time. This deals with 301 error from using http://api.alexandria.io/payproc.

--- src/app/js/media.js
+++ src/app/js/media.js
@@ -2,8 +2,8 @@ var day_avg = false;
 var delay = 5000;
 var keepHash;
 var mainFile;
-var URL_RECV = window.location.protocol+"//api.alexandria.io/payproc/receive";
-var URL_GETRECVD = window.location.protocol+"//api.alexandria.io/payproc/getreceivedbyaddress/";
+var URL_RECV = "https://api.alexandria.io/payproc/receive";
+var URL_GETRECVD = "https://api.alexandria.io/payproc/getreceivedbyaddress/";
 
 window.doMountMediaBrowser = function (el, data) {
     console.log (el, data);
