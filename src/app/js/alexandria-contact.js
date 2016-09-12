80c46b25cfae6c4f6102d7e2716c54de94fc3c80
copernicus-mogley <ryan@9wizards.com>
2016-09-12 19:09:21 +0200

    Add Slack invite link

--- src/app/js/alexandria-contact.js
+++ src/app/js/alexandria-contact.js
@@ -1,5 +1,3 @@
-/*
 $(document).ready(function(){
-	$('#app-footer').prepend('<div id="contact-tab"><div><div class="left">Questions? Comments? Want to help?<br /><a href="http://alexandria.io:3000/" target="_blank">Join us in Slack</a></div><a href="http://alexandria.io:3000/" target="_blank" class="pull-right" style="margin: 0 5px; opacity: .5"><img src="img/SlackMarkBlack-Web-sm.png" /></a></div></div>');
+	$('#app-footer').prepend('<div id="contact-tab"><div><div class="left">Questions? Comments? Want to help?<br /><a href="http://alexandria.io:3000/" target="_blank">Join us in Slack</a></div><a href="http://dloaslack.bitspill.net/" target="_blank" class="pull-right" style="margin: 0 5px; opacity: .5"><img src="img/SlackMarkBlack-Web-sm.png" /></a></div></div>');
 });
-*/
